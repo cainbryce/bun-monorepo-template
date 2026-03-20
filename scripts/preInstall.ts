d644ae0200239e8e40e2f1190be9cb0436418ff3
cainbryce <cainbryce208@gmail.com>
2026-03-20 03:52:15 -0600

    Simplify prompt value assignment in preInstall script
    Remove unnecessary `.trim()` calls and intermediate variable
    assignments.
    
    Signed-off-by: cainbryce <cainbryce208@gmail.com>

--- scripts/preInstall.ts
+++ scripts/preInstall.ts
@@ -7,11 +7,8 @@ const _sw = stdout.writer({ highWaterMark: 512 })
 const _se = stderr.writer({ highWaterMark: 512 })
 const _si = stdin.writer({ highWaterMark: 512 })
 
-const _rootPkg = (prompt(`Enter your root package name:`, "") ?? "").trim()
-
-const _authorName = (
-	prompt(`Enter your name for use as author:`, "") ?? ""
-).trim()
+const _rootPkg = prompt(`Enter your root package name:`, "") ?? ""
+const _authorName = prompt(`Enter your name for use as author:`, "") ?? ""
 
 _sw.write(`${_rootPkg}@${_authorName}`)
 _se.write(`${_rootPkg}@${_authorName}`)
